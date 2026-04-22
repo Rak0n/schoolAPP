9cc3479f5a0f1ab782cdf36b86d4ba501a1af4f5
Rak0n <gulluscio.giova@gmail.com>
2026-04-22 19:37:33 +0200

    Create curiosita.py

---
+++ curiosita.py
@@ -0,0 +1,21 @@
+# curiosita.py
+# Questo file contiene tutte le curiosità che appariranno quando 
+# il bambino posiziona correttamente un oggetto.
+
+testi_curiosita = {
+    # PALEOLITICO
+    "item_pal_1": "Le tende venivano fatte con rami e persino grandi ossa di mammut, coperte da pelli di animali cucite insieme per ripararsi dal freddo intenso!",
+    "item_pal_2": "Cacciavano in gruppo grandi animali per avere carne per tutto il clan. Le lance di legno avevano punte fatte di pietra scheggiata super tagliente.",
+    "item_pal_3": "Mentre alcuni cacciavano, altri raccoglievano radici, bacche, uova e miele. Era un lavoro difficilissimo ma fondamentale per non restare senza cibo!",
+    "item_pal_4": "L'amigdala era il 'coltellino svizzero' della preistoria! Aveva una forma a mandorla per essere impugnata bene: serviva per tagliare la carne, scavare e pulire le pelli.",
+    "item_pal_5": "Il fuoco fu una scoperta rivoluzionaria! Non serviva solo per scaldarsi e cuocere il cibo, ma anche per tenere lontani i predatori feroci durante la notte.",
+    "item_pal_6": "Nelle grotte ci si riparava durante le glaciazioni. È proprio sui muri di queste grotte che gli uomini preistorici dipingevano gli animali che volevano cacciare!",
+
+    # NEOLITICO
+    "item_neo_1": "I primi semi coltivati furono grano e orzo. Gli esseri umani capirono che piantando un seme nella terra nasceva una nuova pianta: una vera magia!",
+    "item_neo_2": "I vasi di ceramica si ottenevano modellando l'argilla e cuocendola nel fuoco. Erano importantissimi per conservare i cereali per l'inverno o trasportare l'acqua.",
+    "item_neo_3": "I mattoni erano fatti di fango, argilla e paglia, seccati al sole. Visto che ora coltivavano i campi, non serviva più viaggiare, quindi le case divennero fisse e robuste.",
+    "item_neo_4": "Invece di rincorrere le prede, impararono ad addomesticare pecore, capre, mucche e cani. Così avevano sempre latte, lana e aiuto nel lavoro!",
+    "item_neo_5": "La pietra levigata era liscia perché veniva sfregata a lungo con la sabbia e l'acqua. Non avendo punti deboli, era molto più resistente per tagliare gli alberi.",
+    "item_neo_6": "I villaggi nascevano quasi sempre vicino ai fiumi o ai laghi, perché serviva tanta acqua dolce per bere, irrigare i campi coltivati e dissetare gli animali."
+}
